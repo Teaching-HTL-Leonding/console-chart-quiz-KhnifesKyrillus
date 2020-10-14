7119a75b82c845fd01cbb8a8e1444d5424a7926b
Khnifes Kyrillus <khnifes.kyrillus@gmail.com>
2020-10-14 16:44:59 +0200

    finished

--- ConsoleChart/ConsoleChart/Program.cs
+++ ConsoleChart/ConsoleChart/Program.cs
@@ -21,16 +21,14 @@ namespace ConsoleChart
             string[] categories = line.Split('\t');
             var textIndex = Array.IndexOf(categories, args[0]);
             var valueIndex = Array.IndexOf(categories, args[1]);
+
             line = Console.ReadLine();
             while (true)
             {
                 if (string.IsNullOrEmpty(line)) break;
 
-                var value = 0;
-                if (textIndex < line.Split('\t').Length && valueIndex < line.Split('\t').Length)
-                    value = int.Parse(line.Split('\t')[valueIndex]);
 
-                items.Add(new ChartItem(line.Split('\t')[textIndex], value)
+                items.Add(new ChartItem(line.Split('\t')[textIndex], int.Parse(line.Split('\t')[valueIndex]))
                 );
                 line = Console.ReadLine();
             }
